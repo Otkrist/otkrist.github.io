1090496a3afa4a4ad4a8aa0ddd12637f5c1818b0
Otkrist Gupta <otkrist@mit.edu>
2016-02-27 20:40:06 -0500

    Minor

---
+++ julia.js
@@ -0,0 +1,15 @@
+images = [
+  "img/Julia_portrait_2.jpeg",
+  "img/Julia_portrait_3.jpeg",
+  "img/Julia_portrait_4.jpeg",
+  "img/Julia_portrait_6.jpeg"
+];
+
+window.onload = function() {
+  console.log("here here")!
+  var imagenumber = 4 ;
+  var randomnumber = Math.random() ;
+  var rand1 = Math.round( (imagenumber-1) * randomnumber) + 1 ;
+  
+}
+
