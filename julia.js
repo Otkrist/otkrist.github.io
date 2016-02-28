75b14dc9b8425e370bfe0e6ba148e5acf34eb796
Otkrist Gupta <otkrist@mit.edu>
2016-02-28 02:22:02 -0500

    Minor

--- julia.js
+++ julia.js
@@ -19,13 +19,13 @@ window.onload = function() {
 function drawTheSet(canvas) {
   var canvas=document.getElementById("myCanvas");
   var ctx=canvas.getContext("2d");
-  var imgData=ctx.createImageData(100,100);
+  var imgData=ctx.createImageData(600,600);
   for (var i=0;i<imgData.data.length;i+=4) {
-    imgData.data[i+0]=255;
+    imgData.data[i+0]=0;
     imgData.data[i+1]=0;
-    imgData.data[i+2]=0;
+    imgData.data[i+2]=255;
     imgData.data[i+3]=255;
   }
-  ctx.putImageData(imgData,10,10);
+  ctx.putImageData(imgData,20,50);
 }
 
